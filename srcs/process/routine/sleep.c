a93b26835ede46cfa89297030aa0fcf4c4e5985d
Martin <mrt.thonon@gmail.com>
2023-07-10 21:27:39 +0200

    update

--- srcs/process/routine/sleep.c
+++ srcs/process/routine/sleep.c
@@ -5,5 +5,5 @@ void ft_sleep(t_thread_args *philosopher);
 void ft_sleep(t_thread_args *philosopher)
 {
    usleep(5000000);
-   printf("It is %lu and pilosopher n °%zu is sleeping\n", ft_get_time_ms(), philosopher->node->index);
+   printf("It is %llu and pilosopher n °%zu is sleeping\n", ft_get_time_ms(), philosopher->node->index);
 }
