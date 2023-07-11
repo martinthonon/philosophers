f7c4db7253b54f689b3b0651997b497b3ee0918c
martinthonon <mrt.thonon@gmail.com>
2023-07-11 17:14:15 +0200

    update

--- srcs/process/routine/fork.c
+++ srcs/process/routine/fork.c
@@ -7,6 +7,7 @@ bool ft_put_fork(t_thread_args *philosopher);
 bool ft_take_fork(t_thread_args *philosopher)
 {
     (void)philosopher;
+    printf("test\n");
     //pthread_mutex_lock(&philosopher->container->n_fork[philosopher->node->index]);
     return (false);
 }
