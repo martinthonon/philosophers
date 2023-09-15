6e3e3d907b77a6dbd47590d3896c6378b448c20d
martinthonon <mrt.thonon@gmail.com>
2023-09-15 17:14:42 +0200

    refactor

---
+++ srcs/process/sub_routine.c
@@ -0,0 +1,6 @@
+#include "philo.h"
+
+void ft_sub_routine(t_)
+static bool	ft_thread_start(t_dllist *container, t_thread_args *philosopher,
+				t_dllist_node *node);
+static bool	ft_thread_join(t_thread_args *philosopher, size_t size);
