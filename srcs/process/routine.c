e4a65d1d2b7350a42aba8ebb4b2ba7ea6eb82533
martinthonon <mrt.thonon@gmail.com>
2023-09-25 18:44:44 +0200

    update

--- srcs/process/routine.c
+++ srcs/process/routine.c
@@ -24,11 +24,13 @@ void *ft_routine(void *arg)
 static bool ft_eat(t_thread_args *philosopher)
 {
     ft_atomic_print(philosopher, EATING);
+    philosopher->node->time_till_last_meal = ft_get_time_ms();
+    ft_usleep(philosopher->container->time_to_eat);
     return (false);
 }
 
 static bool ft_sleep(t_thread_args *philosopher)
 {
-    (void)philosopher;
+    ft_usleep(philosopher->container->time_to_sleep);
     return (false);
 }
